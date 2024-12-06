b387910baa55f6bd514e814eb010dbe53528caf1
aniket-kangane <aniketkangane@pepsales.co>
2024-12-06 12:12:26 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST"],
+  })
+);
+
+app.get("/", (req, resp) => {
+  resp.status(200).json({
+    message: "Hello from Ec2 nodejs server",
+  });
+});
+
+app.listen(8080, () => {
+  console.log("server is running on port 8080");
+});
