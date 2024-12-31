b71d68995b3571ec68c60c6c9ba0e20d6a95c32f
Aniket Kangane <115465575+Aniket897@users.noreply.github.com>
2024-12-31 10:19:42 +0530

    add: health route

--- index.js
+++ index.js
@@ -21,6 +21,12 @@ app.get("/", (req, resp) => {
   });
 });
 
+app.get("/health" , (req , resp) => {
+  resp.status(200).json({
+    message: "Server is up and running",
+  });
+}
+
 app.listen(8080, () => {
   console.log("server is running on port 8080");
 });
